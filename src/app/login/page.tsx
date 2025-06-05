fc8e52e7e279800406df3eaf7101715386bd5532
sdwianto <sdwianto@gmail.com>
2025-06-05 12:37:42 +0700

    initial commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
-import { Button } from '@/components/ui/Button';
+import { Button } from '@/components/ui/button';
 
 import { login } from '@/lib/api/auth';
 
